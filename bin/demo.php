a06d72e005d71e191c2f336d60396f63881591e8
Asmir Mustafic <goetas@gmail.com>
2016-10-04 15:03:41 +0200

    updated examples and deps

--- bin/demo.php
+++ bin/demo.php
@@ -1,10 +1,17 @@
 <?php
 namespace Example;
 
+
 use GoetasWebservices\SoapServices\SoapClient\ClientFactory;
 use GoetasWebservices\SoapServices\SoapCommon\Builder\SoapContainerBuilder;
 use Symfony\Component\VarDumper\VarDumper;
-use WeatherWS\Container\SoapContainer;
+
+// the metadata container
+use Service\Container\SoapContainer;
+
+// soap stubs
+use WeatherWS\SoapStubs\WeatherSoap;
+use Calculator\SoapStubs\CalculatorSoap;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -16,9 +23,21 @@ $metadata = $container->get('goetas_webservices.soap_client.metadata_reader');
 $factory = new ClientFactory($metadata, $serializer);
 
 /**
- * @var $client \WeatherWS\SoapStubs\WeatherSoap
+ * @var $client CalculatorSoap
  */
-$client = $factory->getClient('http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL');
+$client = $factory->getClient('http://www.dneonline.com/calculator.asmx?WSDL', "CalculatorSoap", "Calculator");
+
+$result = $client->add(1,5);
+
+VarDumper::dump($result);
+VarDumper::dump($result->getAddResult());
+
+exit 0;
+
+/**
+ * @var $client WeatherSoap
+ */
+$client = $factory->getClient('http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL', "WeatherSoap", "Weather");
 
 $result = $client->getCityWeatherByZIP("10006");
 
